efab1a7c4dea53ad5c9ed21a93742eb65eb4ba6d
paranoidal97 <LukaszRams1997@gmail.com>
2024-01-03 12:25:17 +0100

    initial commit

---
+++ src/main/java/com/paranoidal97/ProductstMicroservice/ProductstMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.paranoidal97.ProductstMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductstMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductstMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paranoidal97/ProductstMicroservice/ProductstMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paranoidal97.ProductstMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductstMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
